a795157bb721f03206bb5893eda8954a67708877
Shivam Chhapola <shivam.chhapola.007@gmail.com>
2023-07-04 09:04:03 +0530

    donno if this'll fix or not

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,9 @@ const config = {
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
       files: ['./**/*.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': ['warn'],
+      },
     },
   ],
   parser: '@typescript-eslint/parser',
